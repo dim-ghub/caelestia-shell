caelestia-dots/shell

Add favorite and hide toggle buttons to launcher app items

modules/launcher/items/AppItem.qml
@@ -1,8 +1,11 @@
 import QtQuick
+import QtQuick.Controls
 import Quickshell
 import Quickshell.Widgets
 import Caelestia.Config
 import qs.components
+import qs.components.containers
+import qs.components.effects
 import qs.services
 import qs.utils
 import qs.modules.launcher.services
@@ -19,7 +22,9 @@ Item {
     anchors.right: parent?.right
 
     StateLayer {
+        id: stateLayer
         radius: Tokens.rounding.normal
+        acceptedButtons: Qt.LeftButton
         onClicked: {
             Apps.launch(root.modelData);
             root.visibilities.launcher = false;
@@ -47,7 +52,7 @@ Item {
             anchors.leftMargin: Tokens.spacing.normal
             anchors.verticalCenter: icon.verticalCenter
 
-            implicitWidth: parent.width - icon.width - favouriteIcon.width
+            implicitWidth: parent.width - icon.width - 80
             implicitHeight: name.implicitHeight + comment.implicitHeight
 
             StyledText {
@@ -65,24 +70,65 @@ Item {
                 color: Colours.palette.m3outline
 
                 elide: Text.ElideRight
-                width: root.width - icon.width - favouriteIcon.width - Tokens.rounding.normal * 2
+                width: root.width - icon.width - 80 - Tokens.rounding.normal * 2
 
                 anchors.top: name.bottom
             }
         }
 
-        Loader {
-            id: favouriteIcon
-
-            asynchronous: true
+        MouseArea {
+            id: hideIcon
+            width: 32
+            height: 32
             anchors.verticalCenter: parent.verticalCenter
             anchors.right: parent.right
-            active: root.modelData && Strings.testRegexList(GlobalConfig.launcher.favouriteApps, root.modelData.id)
+            hoverEnabled: true
+            onClicked: {
+                const appId = root.modelData?.id;
+                if (!appId) return;
+                const hiddenApps = GlobalConfig.launcher.hiddenApps ? [...GlobalConfig.launcher.hiddenApps] : [];
+                if (Strings.testRegexList(hiddenApps, appId)) {
+                    const idx = hiddenApps.indexOf(appId);
+                    if (idx !== -1) hiddenApps.splice(idx, 1);
+                } else {
+                    hiddenApps.push(appId);
+                }
+                GlobalConfig.launcher.hiddenApps = hiddenApps;
+            }
 
-            sourceComponent: MaterialIcon {
-                text: "favorite"
-                fill: 1
-                color: Colours.palette.m3primary
+            MaterialIcon {
+                anchors.centerIn: parent
+                text: Strings.testRegexList(GlobalConfig.launcher.hiddenApps, root.modelData?.id) ? "visibility_off" : "visibility"
+                color: hideIcon.containsMouse ? Colours.palette.m3primary : Colours.palette.m3outline
+            }
+        }
+
+        MouseArea {
+            id: favIcon
+            width: 32
+            height: 32
+            anchors.verticalCenter: parent.verticalCenter
+            anchors.right: hideIcon.left
+            anchors.rightMargin: Tokens.padding.small
+            hoverEnabled: true
+            onClicked: {
+                const appId = root.modelData?.id;
+                if (!appId) return;
+                const favApps = GlobalConfig.launcher.favouriteApps ? [...GlobalConfig.launcher.favouriteApps] : [];
+                if (Strings.testRegexList(favApps, appId)) {
+                    const idx = favApps.indexOf(appId);
+                    if (idx !== -1) favApps.splice(idx, 1);
+                } else {
+                    favApps.push(appId);
+                }
+                GlobalConfig.launcher.favouriteApps = favApps;
+            }
+
+            MaterialIcon {
+                anchors.centerIn: parent
+                text: Strings.testRegexList(GlobalConfig.launcher.favouriteApps, root.modelData?.id) ? "favorite" : "favorite_border"
+                fill: Strings.testRegexList(GlobalConfig.launcher.favouriteApps, root.modelData?.id) ? 1 : 0
+                color: favIcon.containsMouse ? Colours.palette.m3primary : Colours.palette.m3onSurfaceVariant
             }
         }
     }

dim-ghub/caelestia-shell

modules/launcher/items/AppItem.qml
@@ -1,8 +1,11 @@
 import QtQuick
+import QtQuick.Controls
 import Quickshell
 import Quickshell.Widgets
 import Caelestia.Config
 import qs.components
+import qs.components.containers
+import qs.components.effects
 import qs.services
 import qs.utils
 import qs.modules.launcher.services
@@ -19,7 +22,9 @@ Item {
     anchors.right: parent?.right
 
     StateLayer {
+        id: stateLayer
         radius: Tokens.rounding.large
+        acceptedButtons: Qt.LeftButton
         onClicked: {
             Apps.launch(root.modelData);
             root.visibilities.launcher = false;
@@ -47,7 +52,7 @@ Item {
             anchors.leftMargin: Tokens.spacing.medium
             anchors.verticalCenter: icon.verticalCenter
 
-            implicitWidth: parent.width - icon.width - favouriteIcon.width
+            implicitWidth: parent.width - icon.width - 80
             implicitHeight: name.implicitHeight + comment.implicitHeight
 
             StyledText {
@@ -65,24 +70,65 @@ Item {
                 color: Colours.palette.m3outline
 
                 elide: Text.ElideRight
-                width: root.width - icon.width - favouriteIcon.width - Tokens.rounding.extraLargeIncreased
+                width: root.width - icon.width - 80 - Tokens.rounding.large * 2
 
                 anchors.top: name.bottom
             }
         }
 
-        Loader {
-            id: favouriteIcon
-
-            asynchronous: true
+        MouseArea {
+            id: hideIcon
+            width: 32
+            height: 32
             anchors.verticalCenter: parent.verticalCenter
             anchors.right: parent.right
-            active: root.modelData && Strings.testRegexList(GlobalConfig.launcher.favouriteApps, root.modelData.id)
+            hoverEnabled: true
+            onClicked: {
+                const appId = root.modelData?.id;
+                if (!appId) return;
+                const hiddenApps = GlobalConfig.launcher.hiddenApps ? [...GlobalConfig.launcher.hiddenApps] : [];
+                if (Strings.testRegexList(hiddenApps, appId)) {
+                    const idx = hiddenApps.indexOf(appId);
+                    if (idx !== -1) hiddenApps.splice(idx, 1);
+                } else {
+                    hiddenApps.push(appId);
+                }
+                GlobalConfig.launcher.hiddenApps = hiddenApps;
+            }
 
-            sourceComponent: MaterialIcon {
-                text: "favorite"
-                fill: 1
-                color: Colours.palette.m3primary
+            MaterialIcon {
+                anchors.centerIn: parent
+                text: Strings.testRegexList(GlobalConfig.launcher.hiddenApps, root.modelData?.id) ? "visibility_off" : "visibility"
+                color: hideIcon.containsMouse ? Colours.palette.m3primary : Colours.palette.m3outline
+            }
+        }
+
+        MouseArea {
+            id: favIcon
+            width: 32
+            height: 32
+            anchors.verticalCenter: parent.verticalCenter
+            anchors.right: hideIcon.left
+            anchors.rightMargin: Tokens.padding.small
+            hoverEnabled: true
+            onClicked: {
+                const appId = root.modelData?.id;
+                if (!appId) return;
+                const favApps = GlobalConfig.launcher.favouriteApps ? [...GlobalConfig.launcher.favouriteApps] : [];
+                if (Strings.testRegexList(favApps, appId)) {
+                    const idx = favApps.indexOf(appId);
+                    if (idx !== -1) favApps.splice(idx, 1);
+                } else {
+                    favApps.push(appId);
+                }
+                GlobalConfig.launcher.favouriteApps = favApps;
+            }
+
+            MaterialIcon {
+                anchors.centerIn: parent
+                text: Strings.testRegexList(GlobalConfig.launcher.favouriteApps, root.modelData?.id) ? "favorite" : "favorite_border"
+                fill: Strings.testRegexList(GlobalConfig.launcher.favouriteApps, root.modelData?.id) ? 1 : 0
+                color: favIcon.containsMouse ? Colours.palette.m3primary : Colours.palette.m3onSurfaceVariant
             }
         }
     }